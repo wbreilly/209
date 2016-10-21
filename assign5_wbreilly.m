a516737bb2737ff6369fc62d21061b8c65dcff21
Walter Reilly <wbreilly@ucdavis.edu>
2016-10-20 17:21:09 -0700

    for loops are set up

--- assign5_wbreilly.m
+++ assign5_wbreilly.m
@@ -18,21 +18,52 @@ tdfread(fname);
 
 
 % Your code should print the following information to the MATLAB command window:
-% 1. The number of subjects in the dataset and a list of the unique ...
+%% 1. The number of subjects in the dataset and a list of the unique ...
 %     subject IDs (2 pts)
 
+% unique subjects
+u_sub = unique(Subject);
+% number of subjects
+n_sub = length(u_sub);
+
 % print the number of subjects
-fprintf('# subjects: %d \n\n', length(unique(Subject)))
+fprintf('\n # subjects: %d \n\n', n_sub)
 
 % print the unique subject numbers
-fprintf('\n Subjects \n') % header
-fprintf('%d \n', unique(Subject))
+fprintf(' Subjects \n') % header
+fprintf(' %d \n', u_sub)
 
-% 2. The number of conditions in the data set and a list of the unique...
+%% 2. The number of conditions in the data set and a list of the unique...
 %     condition IDs (2 pts)
-% 3. The number of factors in the data set and a list of the unique...
+
+% uniquie conditions
+u_con = unique(Condition);
+% number of conditions
+n_con = length(u_con);
+
+% print the number of conditions
+fprintf('# conditions: %d \n\n', n_con)
+
+% print the unique subject numbers
+fprintf(' Conditions \n') % header
+fprintf('%d \n', u_con)
+
+%% 3. The number of factors in the data set and a list of the unique...
 %     factor IDs (2 pts)
-% 4. A tab-delimited table specifying the number of trials for each...
+
+% unique factors
+u_fac = unique(Factor);
+% number factors 
+n_fac = length(u_fac);
+
+% print the number of factors
+fprintf('# factors: %d \n\n', n_fac)
+
+% print the unique subject numbers
+fprintf(' Factors \n') % header
+fprintf('%d \n', u_fac)
+
+%% 4. A tab-delimited table specifying the number of trials for each...
 %     subject in each Factor/Condition combination (as in Assignment 4). (2 pts)
 %     1. Here are some hints for constructing the formatting string ...
 %         that you give to fprintf().
@@ -42,6 +73,23 @@ fprintf('%d \n', unique(Subject))
 %             , 5, 6, 8 and 9, the output is Sub05, Sub06, etc. Use the...
 %             fprintf() help to figure out how to use an arbitrary number...
 %             of digits when printing an integer, e.g. 01, 001, or 0001, etc.
+
+for isub = 1:n_sub
+    cur_sub = u_sub(isub);
+    sub_mask = Subject == cur_sub;
+    
+    for ifac = 1:n_fac
+        cur_fac = u_fac(ifac);
+        fac_mask = Factor == cur_fac;
+        
+        for icon = 1:n_con
+            cur_con = u_con(icon);
+            con_mask = Condition == cur_con;
+            
+        end % end icon
+    end % end ifac
+end % end isub
+
 % 5. There are a number of subjects who are missing data for some...
 %     combinations of factor and conditions. For each subject who is...
 %     missing data, print their subject ID and the IDs of the...
@@ -53,7 +101,7 @@ fprintf('%d \n', unique(Subject))
 % 7. The standard error of the mean (SEM) for each subject in each ...
 %     Factor/Condition combination. To calculate the standard error, use...
 %     std() to obtain the standard deviation, and divide this by the square...
-%     root [sqrt()] of the number of trials in that Factor/Condition ...
+%     root [sqrt()] of the number of trials in that Factor/Condition...
 %     combination for that subject. (2 pts)
 % 8. The mean and SEM across subjects for each Factor/Condition ...
 %     combination. Note that the mean here is the mean of individual...
