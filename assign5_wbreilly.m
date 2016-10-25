589b19c46834e7a3ca74e7c90f22bd8c2fa9d1f1
Walter Reilly <wbreilly@ucdavis.edu>
2016-10-25 15:27:15 -0700

    finished homework 5

--- assign5_wbreilly.m
+++ assign5_wbreilly.m
@@ -1,16 +1,8 @@
 % Walter Reilly
-% last update 10_20_!6
+% last update 10_25_16
 % HW 5
 % PSC 209
 
-% Instructions
-% Basic Data Analysis
-% This assignment is essentially the same as Assignment 4, but with a ...
-%     couple of wrinkles.
-% The file for this assignment is named, "txtdata_IDsNotIndices_MissingData.txt' ...
-%     and resides in the "datafiles" subdirectory of the Resources section ...
-%     of this website.
-%  
 
 % read in data
 fname = sprintf('txtdata_IDsNotIndices_MissingData.txt');
@@ -49,7 +41,7 @@ fprintf(' Conditions \n') % header
 fprintf('%d \n', u_con)
 
 %% 3. The number of factors in the data set and a list of the unique...
-%     factor IDs (2 pts)
+%     factor IDs (2 pts)s
 
 % unique factors
 u_fac = unique(Factor);
@@ -74,12 +66,20 @@ fprintf('%d \n', u_fac)
 %             fprintf() help to figure out how to use an arbitrary number...
 %             of digits when printing an integer, e.g. 01, 001, or 0001, etc.
 
+% tell em what's comin
+fprintf('\n Number of Trials  \n\n')
+
+% create header
+h_line = create_hdrline(n_fac, n_con);
+%print header
+fprintf('Subject:\t%s', h_line)
+
 for isub = 1:n_sub
     cur_sub = u_sub(isub);
     sub_mask = Subject == cur_sub;
     
      % Print subject ID info
-    cursub_id = sprintf('Sub%02d', cur_sub);
+    cursub_id = sprintf('\nSub%02d\t', cur_sub);
     fprintf('%s', cursub_id);
     
     for ifac = 1:n_fac
@@ -97,64 +97,244 @@ for isub = 1:n_sub
             num_trials = sum(comp_mask);
             
             % print the trial number
-            fprintf('\t%d \n', num_trials);
+            fprintf('\t%d', num_trials);
             
         end % end icon
     end % end ifac
 end % end isub
 
-% 5. There are a number of subjects who are missing data for some...
+%% 5. There are a number of subjects who are missing data for some...
 %     combinations of factor and conditions. For each subject who is...
 %     missing data, print their subject ID and the IDs of the...
 %     factor/condition combinations in which they are missing data (4 pts)
-% 6. A tab-delimited table specifying the mean score for each subject...
+
+% tell em what's comin
+fprintf('\n\n Missing Data')
+
+% copy of above
+for isub = 1:n_sub
+    cur_sub = u_sub(isub);
+    sub_mask = Subject == cur_sub;
+    
+     % Print subject ID info
+    cursub_id = sprintf('\nSub%02d\t', cur_sub);
+    % fprintf('%s', cursub_id);
+    
+    for ifac = 1:n_fac
+        cur_fac = u_fac(ifac);
+        fac_mask = Factor == cur_fac;
+        
+        for icon = 1:n_con
+            cur_con = u_con(icon);
+            con_mask = Condition == cur_con;
+            
+            % create composite mask
+            comp_mask = sub_mask & fac_mask & con_mask;
+            
+            % sum trials
+            num_trials = sum(comp_mask);
+            
+            if num_trials == 0
+                fprintf('\n %s is missing F%dC%d \n', cursub_id, ifac, icon)
+            end
+              
+        end % end icon
+    end % end ifac
+end % end isub
+
+
+%% 6. A tab-delimited table specifying the mean score for each subject...
 %     in each Factor/Condition combination. For those subjects missing ...
 %     data in a factor/condition combination, print the word NaN rather ...
 %     than a number (4 pt)
-% 7. The standard error of the mean (SEM) for each subject in each ...
+
+% tell em what's comin
+fprintf('\n Means \n\n')
+
+% create header
+h_line = create_hdrline(n_fac, n_con);
+%print header
+fprintf('Subject:\t%s', h_line)
+
+% copy from above, doing means this time
+for isub = 1:n_sub
+    cur_sub = u_sub(isub);
+    sub_mask = Subject == cur_sub;
+    
+     % Print subject ID info
+    cursub_id = sprintf('\nSub%02d\t', cur_sub);
+    fprintf('%s', cursub_id);
+    
+    for ifac = 1:n_fac
+        cur_fac = u_fac(ifac);
+        fac_mask = Factor == cur_fac;
+        
+        for icon = 1:n_con
+            cur_con = u_con(icon);
+            con_mask = Condition == cur_con;
+            
+            % create composite mask
+            comp_mask = sub_mask & fac_mask & con_mask;
+            
+            % sum trials
+            num_trials = sum(comp_mask);
+            
+            % get scores
+            cur_score = Score(comp_mask);
+            
+            %get mean
+            mn_score = mean(cur_score);
+            
+            if num_trials == 0
+                fprintf('\tNaN')
+            else
+                % print the mean
+                fprintf('\t%.3f', mn_score);
+            end
+            
+        end % end icon
+    end % end ifac
+end % end isub
+
+
+%% 7. The standard error of the mean (SEM) for each subject in each ...
 %     Factor/Condition combination. To calculate the standard error, use...
 %     std() to obtain the standard deviation, and divide this by the square...
 %     root [sqrt()] of the number of trials in that Factor/Condition...
 %     combination for that subject. (2 pts)
-% 8. The mean and SEM across subjects for each Factor/Condition ...
+
+
+% tell em what's comin
+fprintf('\n\n Standard Error of the Mean \n\n')
+
+% create header
+h_line = create_hdrline(n_fac, n_con);
+%print header
+fprintf('Subject:\t%s', h_line)
+
+% copy from above, doing means this time
+for isub = 1:n_sub
+    cur_sub = u_sub(isub);
+    sub_mask = Subject == cur_sub;
+    
+     % Print subject ID info
+    cursub_id = sprintf('\nSub%02d\t', cur_sub);
+    fprintf('%s', cursub_id);
+    
+    for ifac = 1:n_fac
+        cur_fac = u_fac(ifac);
+        fac_mask = Factor == cur_fac;
+        
+        for icon = 1:n_con
+            cur_con = u_con(icon);
+            con_mask = Condition == cur_con;
+            
+            % create composite mask
+            comp_mask = sub_mask & fac_mask & con_mask;
+            
+            % sum trials
+            num_trials = sum(comp_mask);
+            
+            % get scores
+            cur_score = Score(comp_mask);
+            
+            %get SEM
+            sem = std(cur_score)/sqrt(num_trials);
+            
+            if num_trials == 0
+                fprintf('\tNaN')
+            else
+                % print the sem
+                fprintf('\t%.3f', sem);
+            end
+            
+        end % end icon
+    end % end ifac
+end % end isub
+
+
+
+%% 8. The mean and SEM across subjects for each Factor/Condition ...
 %     combination. Note that the mean here is the mean of individual...
 %     subject means, and the SEM is the standard error of the individual ...
 %     subject means, not the mean of the individual subject SEMs. (2 pts)
-%  
-% 
-% For this assignment, create a Matlab script called assign5_<userid>.m ...
-%     and make sure it runs.
-% Grading
-% 
-% 
-% In addition to the points listed above for accomplishing each of the ...
-%     steps, additional points will be given for:
-% 1. The code executes without crashing (2 pts). I won't penalize for the ...
-%    datafile not loading properly.
-% 2. The code is generalizable (3 pts). I must be able to take your code,...
-%     run it on a new dataset that has the same basic structure but ...
-%     different numbers of subjects and trials, and get the correct ...
-%     result. See Generalization criteria below.
-% 3. Legibility of code (2pts). I should be able to read through and ...
-%     understand your code without much trouble. Please don't pack all...
-%     your lines of code together. Pack lines together that conceptually...
-%     belong to each other, e.g. for-loops. Use comments to indicate ...
-%     the purpose of lines or groups of related lines, and use brief ...
-%     but meaningful variable names.
-% NOTE: If you did not succeed in getting your code to run cleanly ...
-%     through all the steps of the assignment, but you clearly invested ...
-%     effort in trying to find a solution, credit will be given for ...
-%     creative ways of trying to solve the problem.
-% Generalization criteria
-% Generalizability of your code means that it should run and produce the...
-%     correct output dynamically, i.e. without any intervention on your ...
-%     part, when certain properties of the input dataset change.  In this ...
-%     assignment, your code should accommodate:
-% 
-% 1. Different numbers of subjects.
-% 2. Different numbers of trials per condition for each subject.
-% 3. Different numbers of conditions.
-% 
+
+% printing into a matrix this time
+tbl1 = [];
+
+% tell em what's comin
+fprintf('\n\n Summary Stats (SaNs NaNs) \n\n')
+
+% copy from above, putting means into a matrix
+for isub = 1:n_sub
+    cur_sub = u_sub(isub);
+    sub_mask = Subject == cur_sub;
+    
+     % Print subject ID info
+    cursub_id = sprintf('\nSub%02d\t', cur_sub);
+    
+    for ifac = 1:n_fac
+        cur_fac = u_fac(ifac);
+        fac_mask = Factor == cur_fac;
+        
+        for icon = 1:n_con
+            cur_con = u_con(icon);
+            con_mask = Condition == cur_con;
+            
+            % create composite mask
+            comp_mask = sub_mask & fac_mask & con_mask;
+            
+            % sum trials
+            num_trials = sum(comp_mask);
+            
+            % get scores
+            cur_score = Score(comp_mask);
+            
+            %get mean
+            mn_score = mean(cur_score);
+            
+            if num_trials == 0
+                % update table
+                tbl1 = [tbl1 NaN];
+            else
+                % update table
+                tbl1 = [tbl1 mn_score];
+                
+            end % end if
+        end % end icon
+    end % end ifac
+end % end isub
+
+% number of factor condition combos
+n_ttypes = n_fac * n_con;
+% reshape table from column vector to subject by trial type matrix
+tbl1 = reshape(tbl1, n_ttypes(1), n_sub(1))';
+
+% where will you put across subject means and SEMS
+sum_stats = zeros(2,n_ttypes); 
+
+% calculate mean and sem across subjects
+for itype = 1:n_ttypes
+    % means after removing NaNs
+    sum_stats(1,itype) = nanmean(tbl1(:,itype));
+    % sem after removing NaNs
+    sum_stats(2,itype) = nanstd(tbl1(:,itype))/sqrt(n_sub);
+end % itype
+
+%print the header
+fprintf('\t%s', h_line)
+
+% print the means
+fprintf('\nMeans\t')
+for itype = 1:n_ttypes
+    fprintf('%.3f\t', sum_stats(1,itype));
+end
+
+% print the sems
+fprintf('\nSEMs\t')
+for itype = 1:n_ttypes
+    fprintf('%.3f\t', sum_stats(2,itype));
+end
 
 
 
